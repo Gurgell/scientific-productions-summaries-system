62b461657ece80edf4ee6618efc2f0ff8afc6f0a
Everson Martinez <everson.mimartinez@gmail.com>
2024-06-12 10:44:06 -0300

    Fix: Added Cross Origin notation for graph's endpoints

--- backend/scientific-productions-summaries-system/src/main/java/com/example/scientificproductionssystem/controllers/GraphController.java
+++ backend/scientific-productions-summaries-system/src/main/java/com/example/scientificproductionssystem/controllers/GraphController.java
@@ -7,12 +7,14 @@ import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/graph")
+@CrossOrigin
 public class GraphController {
     @Autowired
     private GraphService graphService;
